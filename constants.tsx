b78be470b58a4a77fc851cb65f299321498ac6df
jefersonbar87 <jefersonbar87@gmail.com>
2026-07-28 20:21:30 -0300

    feat: correção do estoque

--- constants.tsx
+++ constants.tsx
@@ -418,7 +418,7 @@ export const INITIAL_PRODUCTS: Product[] = [
     category: 'Bebida',
     image: 'https://objectstorage.sa-saopaulo-1.oraclecloud.com/n/grodnkjmhsk8/b/fotos-pizzaria/o/cocazero.jpg',
     available: true,
-    stock: 0,
+    stock: 10,
   },
   {
     id: '6oglq4b39',
